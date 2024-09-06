a40f9616405737f7b4375751ee51b39da61ef0e8
Victor Rossi <110697727+VictoRossiPereira@users.noreply.github.com>
2024-09-05 23:52:40 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo:"O Programador Pragmático: De Aprendiz a Mestre.",
+        descricao: "Escrito por Andrew Hunt e David Thomas, a obra oferece uma coleção de dicas e técnicas práticas para aprimorar a qualidade do código, aumentar a produtividade e resolver problemas de forma mais eficiente.",
+        link: "https://www.amazon.com.br/14-H%C3%A1bitos-Desenvolvedores-Altamente-Produtivos-ebook/dp/B08BF7PZZX"
+    },
+    {
+        titulo:"14 Hábitos de Desenvolvedores Altamente Produtivos.",
+        descricao: "O autor, Zeno Rocha, apresenta uma série de práticas e mentalidades que podem transformar a forma como você aborda o desenvolvimento de software. Desde a importância de focar nos fundamentos até a necessidade de buscar constantemente novos conhecimentos, a obra oferece insights valiosos para quem deseja se destacar na área.",
+        link: "https://www.amazon.com.br/14-H%C3%A1bitos-Desenvolvedores-Altamente-Produtivos-ebook/dp/B08BF7PZZX" 
+    },
+    {
+        titulo:"Entendendo Algoritmos: Um guia ilustrado para programadores e outros curiosos.",
+        descricao: "De forma clara e didática, o livro guia o leitor por um universo de instruções passo a passo, revelando como esses mecanismos são a base de qualquer software. Com uma linguagem acessível e repleta de ilustrações, o autor Aditya Y. Bhargava torna o aprendizado sobre algoritmos uma experiência prazerosa, tanto para programadores experientes quanto para aqueles que estão iniciando seus estudos na área.",
+        link: "https://www.amazon.com.br/Entendendo-Algoritmos-Ilustrado-Programadores-Curiosos/dp/8575225634"
+    },
+    {
+        titulo:"JavaScript Moderno: Guia Definitivo.",
+        descricao: "JavaScript: O Guia Definitivo é uma obra de referência essencial para qualquer desenvolvedor que deseje dominar a linguagem JavaScript. Escrito por David Flanagan, este livro abrangente é reconhecido como um dos melhores recursos disponíveis para aprender sobre JavaScript e suas aplicações no desenvolvimento web.",
+        link: "https://www.amazon.com.br/JavaScript-Guia-Definitivo-David-Flanagan/dp/856583719X"
+    },
+    {
+        titulo:"Clean Code: A Handbook of Agile Software Craftsmanship",
+        descricao: "Código Limpo é um guia indispensável para programadores que buscam aprimorar suas habilidades e escrever código de alta qualidade. O livro, escrito por Robert C. Martin (conhecido como Uncle Bob), explora os princípios, padrões e práticas que levam à criação de código limpo, legível e fácil de manter.",
+        link: "https://www.amazon.com/Clean-Code-Handbook-Software-Craftsmanship/dp/0132350882"
+    },
+    {
+        titulo:"Refatoração - Aperfeiçoando o Projeto de Código Existente",
+        descricao: "Refatoração é um guia essencial para desenvolvedores que desejam melhorar a qualidade e a manutenibilidade de seus códigos. Escrito por Martin Fowler, o livro apresenta uma abordagem prática e detalhada para transformar códigos complexos e difíceis de entender em estruturas mais limpas, organizadas e eficientes.",
+        link: "https://www.amazon.com.br/Refatora%C3%A7%C3%A3o-Aperfei%C3%A7oando-Design-C%C3%B3digos-Existentes/dp/857522724"
+    }
+];
